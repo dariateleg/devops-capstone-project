349dfb015dbe8ec383d00866d6d2c4f84af987e9
kotenb <dariateleg@gmail.com>
2023-10-08 15:32:06 -0400

    add an api to get an account'

--- service/routes.py
+++ service/routes.py
@@ -68,7 +68,17 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    """
+    This route will return an account based on the provided id
+    """
+    app.logger.info("Request to read an Account")
+    check_content_type("application/json")
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
